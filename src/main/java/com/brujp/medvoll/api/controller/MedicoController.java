35ec8175ed5a6a0c8d25cbf6d1e84881152f239a
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2023-12-12 19:41:50 -0300

    Paginação default

--- src/main/java/com/brujp/medvoll/api/controller/MedicoController.java
+++ src/main/java/com/brujp/medvoll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,7 +30,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listarMedicos(Pageable paginacao) {
+    public Page<DadosListagemMedico> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         //Converter de uma lista Medico para uma lista de DadosListagemMedico
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
