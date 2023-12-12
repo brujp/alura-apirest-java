10f52361025b03627ac142de72d623a0f1da097d
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2023-12-11 22:54:44 -0300

    Nova migration - Incluindo coluna telefone

--- src/main/java/com/brujp/medvoll/api/model/Medico.java
+++ src/main/java/com/brujp/medvoll/api/model/Medico.java
@@ -23,6 +23,8 @@ public class Medico {
 
     private String email;
 
+    private String telefone;
+
     private String crm;
 
     @Enumerated(EnumType.STRING)
@@ -36,6 +38,7 @@ public class Medico {
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
+        this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
